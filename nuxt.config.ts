44f0eeaa40b593335c68ab050d736b3c028c6d91
Gary Gitton <gary.gitton@gmail.com>
2026-07-17 02:45:46 +0200

    ci: preserve console logs in production build for E2E debugging

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -98,7 +98,7 @@ export default defineNuxtConfig({
         minify: 'terser',
         terserOptions: {
           compress: {
-            drop_console: true,
+            drop_console: false,
             drop_debugger: true
           },
           format: {
